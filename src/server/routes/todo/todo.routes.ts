0bd628455a6a52bb726d72484f07abd78be13df4
sayandas-sd <sayandasmac@gmail.com>
2026-01-29 02:19:46 +0530

    feat: add create post request input by user

--- src/server/routes/todo/todo.routes.ts
+++ src/server/routes/todo/todo.routes.ts
@@ -1,6 +1,6 @@
 import {router, publicProcedure} from "../../trpc";
 import {z} from "zod";
-import { getData, Todo } from "./models";
+import { getData, modelSchema, Todo } from "./models";
 
 
 const todos: Todo[] = [{
@@ -11,12 +11,28 @@ const todos: Todo[] = [{
 }]
 
 export const todoRouter = router({
+    createTodos: publicProcedure
+    .meta({ openapi: {
+        method: 'POST',
+        path: '/create',
+        tags: ['todos'],
+        description: 'create todos'
+    }})
+    .input(z.object({ title: z.string()}))
+    .output(z.object({ todos: modelSchema}))
+    .mutation(({input}) => {
+        todos.push({ id: '1', title: input.title, isCompleted: true})
+        return {
+            todos: {id: '1', title: input.title, isCompleted: true}
+        }
+    }),
+
     getAllTodos: publicProcedure
     .meta({ openapi: { 
         method: 'GET', 
         path: '/todos',
         tags: ['todos'],
-        description: "returns all data",
+        description: 'returns all data',
     }})
     .input(z.undefined())
     .output(getData)
